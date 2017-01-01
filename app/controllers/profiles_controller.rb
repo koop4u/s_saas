496d6a8eabceda4eabab98ba6254994073fd2df9
Stefan <koop4u@gmail.com>
2017-01-01 23:14:18 +0000

    Added show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,20 +7,19 @@ class ProfilesController < ApplicationController
     
     def create
         @user = User.find( params[:user_id] )
-        @profile = @user.build_profile(profile_params)#Trebuie de definit fiecare parametru pentru securitate prin private mai jos!
+        @profile = @user.build_profile(profile_params) #Trebuie de definit fiecare parametru pentru securitate prin private mai jos!
         #salvam forma in baza de date in felul dat:
         if @profile.save
             flash[:success] = "Profile Updated! " #mesaj cu profil Updated
             redirect_to user_path( params[:user_id] ) #redirectioneaza la pagina cu profil completat
-            
         else
             render action: :new
         end
-        
+    end   
         #partea cu declararea!
         private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
         end
-    end
+
 end
