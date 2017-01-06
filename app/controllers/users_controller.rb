35ecff6556509664fd0d19717f296fb3f51e2e1a
Stefan <koop4u@gmail.com>
2017-01-06 00:05:42 +0000

    General Improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
         before_action :authenticate_user!
     
     def index #pentru a arata toate persoanele.
+        @users = User.includes(:profile)
         
     end
     
